0039ff9ded5dd0215f1d15ee7400c45d9225253b
Martin Lehmann <splugs@gmail.com>
2014-02-03 13:29:36 +0100

    Update Talk.java
    Fix formatting and remove unnecessary String assignment.

--- TEDdownloader/src/javaDownloader/Talk.java
+++ TEDdownloader/src/javaDownloader/Talk.java
@@ -2,31 +2,25 @@ package javaDownloader;
 
 public class Talk {
 
-	public String 
-		title		= "",
-		created 	= "",
-		location 	= "",		
-		url 		= "",
-		duration 	= "",		
-		downloadUrl = "";
+	public String
+        title		= "",
+        created 	= "",
+        location 	= "",
+        url 		= "",
+        duration 	= "",
+    	downloadUrl = "";
 
-	public Talk(String htmlData) {
-		String[] htmlDataArr = htmlData.replaceAll("</td>", "").trim().split("<td>");
-				
-		title		= htmlDataArr[3].substring(htmlDataArr[3].indexOf(">") + 1, htmlDataArr[3].indexOf("</a>"));
-		created 	= htmlDataArr[1].trim();
-		location 	= htmlDataArr[2].trim();
-		url			= htmlDataArr[3].substring(htmlDataArr[3].indexOf("talks/") + 6, htmlDataArr[3].indexOf(".html"));
-		duration	= htmlDataArr[4].trim();
-		downloadUrl = htmlDataArr[5].substring(htmlDataArr[5].indexOf("talks/") + 6, htmlDataArr[5].indexOf("-light"));			
-	}
-	
-	public Talk() {
-		title		= "";
-		created 	= "";
-		location 	= "";
-		url			= "";
-		duration	= "";
-		downloadUrl = "";
-	}
+    public Talk(String htmlData) {
+        String[] htmlDataArr = htmlData.replaceAll("</td>", "").trim().split("<td>");
+
+        title       = htmlDataArr[3].substring(htmlDataArr[3].indexOf(">") + 1, htmlDataArr[3].indexOf("</a>"));
+        created     = htmlDataArr[1].trim();
+        location    = htmlDataArr[2].trim();
+        url	        = htmlDataArr[3].substring(htmlDataArr[3].indexOf("talks/") + 6, htmlDataArr[3].indexOf(".html"));
+        duration    = htmlDataArr[4].trim();
+        downloadUrl = htmlDataArr[5].substring(htmlDataArr[5].indexOf("talks/") + 6, htmlDataArr[5].indexOf("-light"));
+    }
+
+    public Talk() {
+    }
 }
